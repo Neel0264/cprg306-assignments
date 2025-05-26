d87836c76033bc3bb9731c402040ad99c588fc36
Neel Moradiya <neeljayeshbhai.moradiya@edu.sait.ca>
2025-05-26 00:57:09 -0600

    Move week-2 folder to root

---
+++ cprg306/cprg306-assignments/week-2/student-info.js
@@ -0,0 +1,15 @@
+// app/week-2/student-info.js
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Neel Moradiya</p>
+      <p>
+        <Link href="https://github.com/Neel0264/cprg306-assignments" target="_blank">
+          GitHub Repository
+        </Link>
+      </p>
+    </div>
+  );
+}
