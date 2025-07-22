4538bcd58c89775bf79d67911979ec5c1fea85cc
Neel Moradiya <neeljayeshbhai.moradiya@edu.sait.ca>
2025-07-21 19:57:54 -0600

    Fixed Firebase init and successful build

--- app/_utils/firebase.js
+++ app/_utils/firebase.js
@@ -1,7 +1,8 @@
+// app/_utils/firebase.js
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
- 
+
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -9,10 +10,11 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
- 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth };
