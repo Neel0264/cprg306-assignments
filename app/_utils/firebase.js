42e92a3f788638f76a5528d1cc20a6cafd122157
Neel Moradiya <neeljayeshbhai.moradiya@edu.sait.ca>
2025-07-21 20:07:54 -0600

    Final Week 10 shopping list setup

--- app/_utils/firebase.js
+++ app/_utils/firebase.js
@@ -1,7 +1,5 @@
-// app/_utils/firebase.js
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -12,9 +10,6 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
-const db = getFirestore(app);
-const auth = getAuth(app);
-
-export { db, auth };
+export const db = getFirestore(app);
